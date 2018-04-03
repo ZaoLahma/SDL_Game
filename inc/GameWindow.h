70b622e5ca3f2f2367b02b82a9dd69f73df9c874
ZaoLahma <thyberg.jan@gmail.com>
2018-04-03 18:18:46 +0200

    Create window context begin

--- inc/GameWindow.h
+++ inc/GameWindow.h
@@ -1,14 +1,18 @@
 #ifndef HEADER_GAME_WINDOW
 #define HEADER_GAME_WINDOW
 
+struct SDL_Window;
+
 class GameWindow
 {
 private:
+  SDL_Window* window;
 
 protected:
 
 public:
   GameWindow();
+  ~GameWindow();
 };
 
 #endif
--- src/GameWindow.cpp
+++ src/GameWindow.cpp
@@ -1,7 +1,26 @@
 #include "GameWindow.h"
 #include <iostream>
+#include <SDL2/SDL.h>
 
 GameWindow::GameWindow()
 {
   std::cout<<"GameWindow(); called"<<std::endl;
+  window = SDL_CreateWindow(
+                              "SDL_GAME",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              640,
+                              480,
+                              SDL_WINDOW_OPENGL
+                            );
+  if(NULL == window)
+  {
+    std::cout<<"Failed to create SDL window: %s"<<SDL_GetError()<<std::endl;
+  }
+}
+
+GameWindow::~GameWindow()
+{
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
